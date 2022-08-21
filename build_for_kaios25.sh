7f0868a3e535029c4c10054a84d38cf3e778b2fb
Avraham Appel <aappel248@gmail.com>
2022-08-21 15:04:01 -0400

    Trying to optimize rollup call so we don't have to run it twice

--- build_for_kaios25.sh
+++ build_for_kaios25.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # Build for KaiOS 2.5
+
 function run () {
-    echo "> $@"
+    echo "> $*"
     $@
 }
 
 # Convert WASM to JS
-for w in $TRUNK_STAGING_DIR/*_bg.wasm
+for w in "$TRUNK_STAGING_DIR"/*_bg.wasm
 do
-    run wasm2js $w -o "$w.js"
-    # TODO is "debug" a valid env for rollup?
-    run rollup --environment $TRUNK_PROFILE -i "$w.js" -o "$w.js"
+    run wasm2js "$w" -o "$w.js"
     wrapper="${w%_bg.wasm}.js"
-    run rollup --environment $TRUNK_PROFILE -i "$wrapper" -o "${wrapper%.js}.wrapper.js"
+    # TODO we will probably need a config for this as CLI options don't seem to do the trick
+    # TODO is "debug" a valid env for rollup?
+    # TODO compile for kaios 2.5 browser which is actually the point here
+    run rollup --environment "$TRUNK_PROFILE" -i "$w.js" -i "$wrapper" --entryFileNames "[name].es5.js"
 done
 
 # Still have to do this,but should be shim script and scripts above added as nomodule scripts in the source html file
+# define my scripts
+# get heade of file (prob until last line)
+# inject my scripts
+# add back last line
